1d937d4456580c583665e040333d58d515e223f1
bartoszposzelezny <bartoszposzelezny@gmail.com>
2026-01-25 12:27:50 +0100

    login form fixes

--- templates/admin/login.html.php
+++ templates/admin/login.html.php
@@ -6,23 +6,22 @@ ob_start(); ?>
 
     <div class="admin-grid">
     <div class="admin-card">
-        <div class="genre-form">
+        <div class="login-form">
         <form onsubmit="event.preventDefault(); SendToAdmin();">
-            <input type="text" id="accessCode" placeholder="Kod dostępu"/>
-
-            <button type="submit" class="btn-primary">Prześlij</button>
+            <input type="password" id="accessCode" placeholder="Kod dostępu"/>
+            <button type="submit" class="btn-primary">Zaloguj</button>
         </form>
     </div>
     </div>
     </div>
 <script>
 function SendToAdmin(){
-    console.log(document.getElementById("accessCode").value)
     if (document.getElementById("accessCode").value==="Admin123!"){
         document.cookie="role=admin; path=/";
         window.location.href = "index.php?action=admin-index";
+    } else{
+        alert("Złe hasło");
     }
-    else{console.log("Złe hasło");}
 
 }
 </script>
